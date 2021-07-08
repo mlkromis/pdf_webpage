43de54db8cc25fef93269d222a1c54d47d48d5ab
Maria Kromis <mariakromis@gmail.com>
2021-07-08 12:28:01 -0500

    updating to remove dead code

--- pdf-coding-challenge/app/controllers/application_controller.rb
+++ pdf-coding-challenge/app/controllers/application_controller.rb
@@ -1,8 +1,3 @@
 class ApplicationController < ActionController::Base
-  def show
-    user = User.find(params[:id])
-    respond_to do |format|
-      format.pdf { send_file TestPdfForm.new(user).export, type: 'application/pdf' }
-    end
-  end
+  protect_from_forgery with: :exception
 end
